c9f61a25d1e5ad98e2693a54e563c0d147c51e81
CWinarski <Winarski70@yahoo.com>
2018-03-14 20:25:17 -0400

    Added Sets Notes

--- src/main/java/CollectionsPractice/Animal.java
+++ src/main/java/CollectionsPractice/Animal.java
@@ -1,5 +1,7 @@
 package CollectionsPractice;
 
+import java.util.Objects;
+
 public class Animal{
     private String species;
     private String habitat;
@@ -16,5 +18,22 @@ public class Animal{
 
     public String getHabitat() {
         return habitat;
+    }
+
+
+    // equals and hashcode are used for comparing objects in hashmap/hashset
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(species, animal.species) &&
+                Objects.equals(habitat, animal.habitat);
+    }
+
+    @Override
+    public int hashCode() {
+        //returns has of species and habitat
+        return Objects.hash(species, habitat);
     }
 }
--- src/main/java/CollectionsPractice/SetsExperiment.java
+++ src/main/java/CollectionsPractice/SetsExperiment.java
@@ -11,12 +11,14 @@ public class SetsExperiment {
 
     public static void main(String[] args) {
         Set<Animal> hashSet = new HashSet<Animal>();
+
         /*
         -calls hashCode on element and looks up location
         -good general purpose and resize
         -requirement that hashcode of object has to equal hashcode of other object
         -you have to make your own custom own equals method
             ex: result * result + obj,hashCode();
+            can generate in ide ,equals w/ hashcode
          */
 
         Set<Animal> treeSet = new TreeSet<Animal>();
