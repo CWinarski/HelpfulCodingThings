dc46119571be49d71c63177b13482c01908569f5
CWinarski <Winarski70@yahoo.com>
2018-03-18 15:36:00 -0400

    Added Reflections Stuff

---
+++ src/main/java/GeneralExperiments.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+public class GeneralExperiments {
+    Object newArray = Array.newInstance(componentType, newLength);
+
+
+    public static Object goodCopyOf(Object a, int newLength) {
+        Class cl = a.getClass();
+        if (!cl.isArray()) return null;
+        Class componentType = cl.getComponentType();
+        int length = Array.getLength(a);
+        Object newArray = Array.newInstance(componentType, newLength);
+        System.arraycopy(a, 0, newArray, 0, Math.min(length, newLength));
+        return newArray;
+    }
+}
+
+
